1b30683e7eb08e2a1a183d637d93be6487e2c6fb
kenorb <kenorb@users.noreply.github.com>
2020-11-27 23:45:34 +0000

    Fixes MQL5 compilation errors of enum

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_RSI_Params_M1 : Indi_RSI_Params {
   Indi_RSI_Params_M1() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M1) {
-    applied_price = 0;
-    period = 12;
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    period = 10;
     shift = 0;
   }
 } indi_rsi_m1;
--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_RSI_Params_M15 : Indi_RSI_Params {
   Indi_RSI_Params_M15() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M15) {
-    applied_price = 0;
+    applied_price = (ENUM_APPLIED_PRICE)0;
     period = 12;
     shift = 0;
   }
