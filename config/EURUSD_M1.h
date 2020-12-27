07e608a17109e279dc3c78aa0e528a7acfdf6ad5
kenorb <kenorb@users.noreply.github.com>
2020-12-27 23:56:06 +0000

    config: Sets applied_price to 1

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_RSI_Params_M1 : Indi_RSI_Params {
   Indi_RSI_Params_M1() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M1) {
-    applied_price = (ENUM_APPLIED_PRICE)0;
+    applied_price = (ENUM_APPLIED_PRICE)1;
     period = 10;
     shift = 0;
   }
