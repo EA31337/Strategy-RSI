ddd1104227cb5e0829d273794442d09eab4009f9
kenorb <kenorb@users.noreply.github.com>
2020-12-29 21:13:17 +0000

    Optimize PriceStopLevel (pf=0.74; dd=11.15; tt=81)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -24,7 +24,7 @@ struct Stg_RSI_Params_M5 : StgParams {
     signal_close_method = 0;
     signal_close_level = 30;
     price_stop_method = 0;
-    price_stop_level = 15;
+    price_stop_level = 35;
     tick_filter_method = 1;
     max_spread = 0;
   }
