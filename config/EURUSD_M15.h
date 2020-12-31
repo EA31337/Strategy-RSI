45359a1ea7d060661af591e73b36d2909952cc9c
kenorb <kenorb@users.noreply.github.com>
2020-12-31 14:13:48 +0000

    config: Updates params

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -7,7 +7,7 @@
 struct Indi_RSI_Params_M15 : Indi_RSI_Params {
   Indi_RSI_Params_M15() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M15) {
     applied_price = (ENUM_APPLIED_PRICE)1;
-    period = 4;
+    period = 12;
     shift = 0;
   }
 } indi_rsi_m15;
@@ -17,12 +17,12 @@ struct Stg_RSI_Params_M15 : StgParams {
   // Struct constructor.
   Stg_RSI_Params_M15() : StgParams(stg_rsi_defaults) {
     lot_size = 0;
-    signal_open_method = 63;
+    signal_open_method = -48;
     signal_open_filter = 1;
     signal_open_level = 30;
     signal_open_boost = 0;
     signal_close_method = 0;
-    signal_close_level = 30;
+    signal_close_level = 10;
     price_stop_method = 0;
     price_stop_level = 15;
     tick_filter_method = 1;
--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_RSI_Params_M5 : Indi_RSI_Params {
   Indi_RSI_Params_M5() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M5) {
-    applied_price = (ENUM_APPLIED_PRICE)1;
-    period = 12;
+    applied_price = (ENUM_APPLIED_PRICE)2;
+    period = 8;
     shift = 0;
   }
 } indi_rsi_m5;
@@ -17,7 +17,7 @@ struct Stg_RSI_Params_M5 : StgParams {
   // Struct constructor.
   Stg_RSI_Params_M5() : StgParams(stg_rsi_defaults) {
     lot_size = 0;
-    signal_open_method = -60;
+    signal_open_method = -63;
     signal_open_filter = 1;
     signal_open_level = 30;
     signal_open_boost = 0;
